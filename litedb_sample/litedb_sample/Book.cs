295addc30a9630895ddec4d9fcb7090de3321a92
wlson2 <158519934+wlson2@users.noreply.github.com>
2024-11-22 13:56:41 -0400

    litedb

---
+++ litedb_sample/litedb_sample/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace litedb_sample
+{
+     public class Book
+    {
+
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+    }
+}
---
+++ litedb_sample/litedb_sample/Program.cs
@@ -0,0 +1,33 @@
+﻿using LiteDB;
+using litedb_sample;
+
+using (var db = new LiteDatabase(@"MyData.db"))
+{
+    var books = db.GetCollection<Book>("books");
+    var booksToInsert = new List<Book>();
+
+    var book = new Book
+    {
+        Title = "The Past",
+        Author = "jhon k",
+        Year = 1949
+    };
+    books.Insert(book);
+
+    var book2 = new Book
+    {
+        Title = "The Present",
+        Author = "Carl Sagan",
+        Year = 2024
+    };
+    booksToInsert.Add(book2);
+    var book3 = new Book
+    {
+        Title = "The Future",
+        Author = "Henry Cavil",
+        Year = 2030
+    };
+    booksToInsert.Add(book3);
+
+    books.InsertBulk(booksToInsert);
+} 
